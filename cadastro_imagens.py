066af1d3572cefb316d430208e053070a6757164
Davi Hilário <davihilario23@gmail.com>
2024-10-20 18:50:37 -0300

    feat: capturando nome da imagem e o objeto imagem vindos do front end

--- cadastro_imagens.py
+++ cadastro_imagens.py
@@ -1,19 +1,26 @@
 import boto3
-import json
 
 def lambda_handler(event, context):
+    
+    nome_imagem = event['nomeImagem']
+    imagem = event['imagem']
+    
     try:  
         s3 = boto3.resource('s3')
-        bucket = s3.Bucket("")
-        bucket.upload_file("")
+        bucket = s3.Bucket("s3-02231010")
+        bucket.put_object(key=nome_imagem, body=imagem)
         
         return {
-            "statusCode": 200,
-            "body": json.dumps("Imagem armazenada com sucesso!")
+            'status': 200,
+            'params': event,
+            'valid': True,
+            'result': 'Upload da imagem para o S3 realizado com sucesso!'
         }
         
     except Exception as e:
         return {
-            "statusCode": 400,
-            "body": json.dumps("Erro ao fazer upload da imagem para o S3: " + e)
+            'status': 400,
+            'params': event,
+            'valid': True,
+            'result': "Erro ao fazer upload da imagem para o S3: " + e
         }
