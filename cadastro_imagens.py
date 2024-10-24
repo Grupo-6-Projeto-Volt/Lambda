780e4ebd55d4ee90fe113df44d29b1d99830ad42
Davi Hilário <davihilario23@gmail.com>
2024-10-24 18:38:34 -0300

    feat: codigo lambda finalizado

--- cadastro_imagens.py
+++ cadastro_imagens.py
@@ -1,5 +1,4 @@
 import boto3
-import json
 
 def lambda_handler(event, context):
     
@@ -7,21 +6,28 @@ def lambda_handler(event, context):
     imagem = event['imagem']
     
     try:  
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket("s3-02231010")
-        bucket.put_object(key=nome_imagem, body=imagem)
+
+        s3 = boto3.client('s3')
+
+        bucket = "s3-02231010" 
+        zone = s3.get_bucket_location(Bucket=bucket)["LocationConstraint"] 
+
+        zone = zone if zone != None else 'us-east-1'
+
+        s3.put_object(Bucket=bucket, Key=nome_imagem, Body=imagem)
         
-        return json.dumps({
+        return {
             'status': 200,
             'params': event,
             'valid': True,
+            'response': f'https://{bucket}.s3.{zone}.amazonaws.com/{nome_imagem}',
             'result': 'Upload da imagem para o S3 realizado com sucesso!'
-        })
-        
+        }
     except Exception as e:
-        return json.dumps({
+        return {
             'status': 400,
             'params': event,
             'valid': False,
-            'errorMessage': e
-        })
+            'response': '',
+            'result': f'Erro ao fazer upload da imagem para a S3: {str(e)}'
+        }
